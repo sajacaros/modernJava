48aa393f68f028e1a41704b2f87eed17c0b39d3d
dukim <dukim@bnpinnovation.com>
2020-05-05 22:47:49 +0900

    init

---
+++ src/test/java/com/study/modern/ModernApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.modern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModernApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
