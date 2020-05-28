b39528d741c2794df9a9112698e92ee10d326d3f
sajacaros <sajacall82@naver.com>
2020-05-28 10:06:17 +0900

    수정

--- src/main/java/com/study/modern/ch07/WordCounter.java
+++ src/main/java/com/study/modern/ch07/WordCounter.java
@@ -11,46 +11,26 @@ import java.util.stream.Stream;
 public class WordCounter {
     private final int counter;
     private final boolean lastSpace;
-    private final List<Character> word = new ArrayList();
 
     public WordCounter(int counter, boolean lastSpace) {
         this.counter = counter;
         this.lastSpace = lastSpace;
     }
 
-    public WordCounter(int counter, boolean lastSpace, List<Character> word) {
-        this.counter = counter;
-        this.lastSpace = lastSpace;
-        this.word.addAll(word);
-    }
-
-    public WordCounter(int counter, boolean lastSpace, Character c) {
-        this.counter = counter;
-        this.lastSpace = lastSpace;
-        this.word.add(c);
-    }
     public WordCounter accumulate(Character c) {
         if (Character.isWhitespace(c)) {
             return lastSpace ?
                 this :
                 new WordCounter(counter, true);
         } else {
-            if(lastSpace) {
-                return new WordCounter(counter+1, false, c);
-            } else {
-                word.add(c);
-                return this;
-            }
+            return lastSpace ?
+                new WordCounter(counter+1, false):
+                this;
         }
     }
     public WordCounter combine(WordCounter wordCounter) {
-        int left = this.word.isEmpty()?0:this.counter;
-        int right = wordCounter.word.isEmpty()?0:wordCounter.counter;
-        log.info("left : {}, word: {}, right: {}, word: {}", left, this.word, right, wordCounter.word);
-        List<Character> words = new ArrayList(this.word);
-        words.addAll(wordCounter.word);
-        return new WordCounter(left+right,
-            wordCounter.lastSpace, words);
+        return new WordCounter(this.counter + wordCounter.counter,
+            wordCounter.lastSpace);
     }
     public int getCounter() {
         return counter;
@@ -58,8 +38,8 @@ public class WordCounter {
 
     public static void main(String[] args) {
         final String SENTENCE =
-            " Nel   mezzo del cammin  di nostra  vita " +
-                "mi  ritrovai in una  selva oscura" +
+            " Nel   mezzo del cammin   di nostra  vita " +
+                "mi  ritrovai in una    selva oscura" +
                 " ché la  dritta via era   smarrita ";
         Stream<Character> stream = IntStream.range(0, SENTENCE.length())
             .mapToObj(SENTENCE::charAt);
