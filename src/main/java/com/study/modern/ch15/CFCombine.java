d75412a90393af9c355159e5922761ea703580fe
sajacaros <sajacall82@naver.com>
2020-06-16 13:58:16 +0900

    CompletableFuture test

---
+++ src/main/java/com/study/modern/ch15/CFCombine.java
@@ -0,0 +1,41 @@
+package com.study.modern.ch15;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.*;
+
+@Slf4j
+public class CFCombine {
+    public static void main(String[] args)
+        throws ExecutionException, InterruptedException {
+        log.info("wait for jconsole");
+        Thread.sleep(20000);
+        ExecutorService executorService = new ThreadPoolExecutor(5, 10,
+            0L, TimeUnit.MILLISECONDS,
+            new LinkedBlockingQueue<>());
+        int x = 1337;
+
+        log.info("start");
+        CompletableFuture<Integer> a = new CompletableFuture<>();
+        CompletableFuture<Integer> b = new CompletableFuture<>();
+        CompletableFuture<Integer> c = a.thenCombine(b, (y, z)-> y + z);
+        executorService.submit(() -> a.complete(f(x)));
+        executorService.submit(() -> b.complete(g(x)));
+        log.info("complete : {}", c.get());
+        executorService.shutdown();
+
+        Thread.sleep(30000);
+    }
+
+    private static Integer g(int x) throws InterruptedException {
+        Thread.sleep(30000);
+        log.info("ggggg");
+        return 2;
+    }
+
+    private static int f(int x) throws InterruptedException {
+        Thread.sleep(5000);
+        log.info("fffff");
+        return 1;
+    }
+}
